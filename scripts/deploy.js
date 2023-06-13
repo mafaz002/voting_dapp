5f471b7eee14c5a172b0c9e858d9cca4216b1f8b
mafaz <mafaz.ahsan@gmail.com>
2023-06-13 09:04:10 +0200

    implemented the foundation for voting contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,16 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const votingContract = await ethers.deployContract("Voting", ["mafaz"]);
-  await votingContract.waitForDeployment();
-  console.log(`Address: ${votingContract.target}`);
+  const contract = await ethers.deployContract("Voting");
+  await contract.waitForDeployment();
+
+  await contract.addCandidate("Asia");
+  await contract.addCandidate("Africa");
+  await contract.addCandidate("Europe");
+  await contract.addCandidate("Australia");
+  await contract.addCandidate("Antartica");
+
+  console.log(`Address: ${contract.target}`);
 }
 
 main().catch((error) => {
